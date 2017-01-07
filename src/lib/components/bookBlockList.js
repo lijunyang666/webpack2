119af48cc813421e5541a931f83aed6deef1c191
aicheng <617179368@qq.com>
2017-01-07 17:56:08 +0800

    wangjile

--- src/lib/components/bookBlockList.js
+++ src/lib/components/bookBlockList.js
@@ -8,7 +8,7 @@ var bookBlockList = Vue.extend({
       +'<ul class="nav"><li class="active" v-link="{path: \'/bookBlockList\'}"><span class="circular"></span><span class="title_name">轻小说</span></li><li v-if="showFlag === 999"><span class="circular"></span><span class="title_name">画集</span></li><li v-link="{path: \'/works\'}"><span class="circular"></span><span class="title_name">创建作品</span></li></ul>'
       +'<div class="content"><div class="bookBlockList"><div class="bookBlockList_title"><span class="hr"></span><span class="title">轻小说</span><span class="titleTwo">light novel</span></div>'
       +'<div style="min-height:500px">'
-      +'<ul class="list clear" ><li><div class="vertical-img"><a v-link="{path: \'/works\'}"><img src="../img/90x126.png" class="cover"/></a></div><div class="vertical-title"></div></li><li v-for="obj in bookList"><div class="vertical-img"><div class="state_one">审核</div><div class="state_tow">连载</div><img :src="path.rootPath + obj.bookCoverImage" class="cover"  v-link="{path: \'/catalogue/\' + obj.bookId}"  /></div><div class="vertical-title" title="{{obj.bookName}}">{{obj.bookName}}</div></li></ul></div></div> '
+      +'<ul class="list clear" ><li><div class="vertical-img"><a v-link="{path: \'/works\'}"><img src="../img/90x126.png" class="cover"/></a></div><div class="vertical-title"></div></li><li v-for="obj in bookList"><div class="vertical-img"><div class="state_one">审核中</div><div class="state_tow">连载中</div><img :src="path.rootPath + obj.bookCoverImage" class="cover"  v-link="{path: \'/catalogue/\' + obj.bookId}"  /></div><div class="vertical-title" title="{{obj.bookName}}">{{obj.bookName}}</div></li></ul></div></div> '
       +'</div>'
       +'</div></div></div>'
       ,data: function() {
