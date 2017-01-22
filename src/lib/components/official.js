2940c6de170e0f11bc8fa2647c0fa244346ffe07
aicheng <617179368@qq.com>
2017-01-22 15:40:11 +0800

    a

--- src/lib/components/official.js
+++ src/lib/components/official.js
@@ -5,7 +5,12 @@ import PathList from '../../lib/apis/conf.js';
 var official = Vue.extend({
       template: 
       '<div class="user_info_right"><div class="user_info_title"><div class="titleBlock">Personal message</div><div class="titleBlock_LG">个人消息</div></div><div class="user_info_content"><div class="contribution">'
-      +'<ul class="nav"><li v-link="{path: \'/message\'}"> <div class="mag_number1" style="display:none;" v-show="bookList.reply === 0">{{bookList.reply}}</div><span class="circular"></span><span class="title_name">我的回复</span></li><li  v-link="{path: \'/tellMe\'}"> <div class="mag_number1" style="display:none;" v-show="bookList.reply === 0">{{bookList.reply}}</div><span class="circular"></span><span class="title_name">@我的</span></li><li  v-link="{path: \'/relevant\'}"> <div class="mag_number1" style="display:none;" v-show="bookList.book === 0">{{bookList.book}}</div><span class="circular"></span><span class="title_name">作品相关</span></li><li  v-link="{path: \'/system\'}"> <div class="mag_number1" style="display:none;" v-show="bookList.system === 0">{{bookList.system}}</div><span class="circular"></span><span class="title_name">系统消息</span></li><li class="active"  v-link="{path: \'/official\'}"><div class="mag_number1" style="display:none;" v-show="bookList.official === 0">{{bookList.official}}</div><span class="circular"></span><span class="title_name">官方公共</span></li></ul>'
+      +'<ul class="nav"><li v-link="{path: \'/message\'}">' 
+      +'<div class="mag_number1" v-if="reply > 0">{{reply}}</div><span class="circular"></span><span @click="setreplyFn" class="title_name">我的回复</span></li><li  v-link="{path: \'/tellMe\'}">'
+      +'<div class="mag_number1" style="display:none;" ></div><span class="circular"></span><span class="title_name">@我的</span></li><li  v-link="{path: \'/relevant\'}">' 
+      +'<div class="mag_number1" v-if="book > 0">{{book}}</div><span class="circular"></span><span @click="setbookFn" class="title_name">作品相关</span></li><li  v-link="{path: \'/system\'}">' 
+      +'<div class="mag_number1" v-if="system > 0">{{system}}</div><span class="circular"></span><span @click="setsystemFn" class="title_name">系统消息</span></li><li class="active"  v-link="{path: \'/official\'}">'
+      +'<div class="mag_number1" v-if="official > 0">{{official}}</div><span class="circular"></span><span @click="setofficialFn" class="title_name">官方公共</span></li></ul>'
       +'<div class="content"><div class="bookBlockList" ><div class="bookBlockList_title"><span class="hr"></span><span class="title">官方公共</span><span class="titleTwo">official public</span></div>'
       
       +'<div style="min-height:500px !important">'
@@ -31,6 +36,10 @@ var official = Vue.extend({
             pageCount: 1,
           },
           messageStatus:0,
+          book:'',
+          official:'',
+          reply:'',
+          system:'',
         };
       },
       route: {
@@ -39,6 +48,18 @@ var official = Vue.extend({
         } 
       },
       methods: {
+        setbookFn: function(){
+          this.$set('book', 0);
+        },
+        setofficialFn: function(){
+          this.$set('official', 0);
+        },
+        setreplyFn: function(){
+          this.$set('reply', 0);
+        },
+        setsystemFn: function(){
+          this.$set('system', 0);
+        },
         setPageCount: function(v) {
           var obj = this.obj;
           obj.pageCount =  v;
@@ -62,6 +83,14 @@ var official = Vue.extend({
             this.$set('bookList', response.data);
             console.log(this.bookList);
             this.setPageCount(response.data.totalPage);
+          });
+          SZXJ.http(this,'get', PathList.getStatus, {}, (response) => {
+            this.userName = response.data.status.userName;
+            this.book = response.data.book;
+            this.official = response.data.official;
+            this.reply = response.data.reply;
+            this.system = response.data.system;
+            this.number1 = response.data.system + response.data.reply + response.data.official + response.data.book;
           });
         },
       },
--- src/lib/components/relevant.js
+++ src/lib/components/relevant.js
@@ -6,11 +6,11 @@ var relevant = Vue.extend({
       template: 
       '<div class="user_info_right"><div class="user_info_title"><div class="titleBlock">Personal message</div><div class="titleBlock_LG">个人消息</div></div><div class="user_info_content"><div class="contribution">'
       +'<ul class="nav"><li v-link="{path: \'/message\'}">'
-      +'<div class="mag_number1" style="display:none;" v-show="bookList.reply === 0">{{bookList.reply}}</div><span class="circular"></span><span class="title_name">我的回复</span></li><li  v-link="{path: \'/tellMe\'}">'
-      +'<div class="mag_number1" style="display:none;" v-show="bookList.reply === 0">{{bookList.reply}}</div><span class="circular"></span><span class="title_name">@我的</span></li><li class="active" v-link="{path: \'/relevant\'}">'
-      +'<div class="mag_number1" style="display:none;" v-show="bookList.book === 0">{{bookList.book}}</div><span class="circular"></span><span class="title_name">作品相关</span></li><li  v-link="{path: \'/system\'}">'
-      +'<div class="mag_number1" style="display:none;" v-show="bookList.system === 0">{{bookList.system}}</div><span class="circular"></span><span class="title_name">系统消息</span></li><li  v-link="{path: \'/official\'}">'
-      +'<div class="mag_number1" style="display:none;" v-show="bookList.official === 0">{{bookList.official}}</div><span class="circular"></span><span class="title_name">官方公共</span></li></ul>'
+      +'<div class="mag_number1" v-if="reply > 0">{{reply}}</div><span class="circular"></span><span @click="setreplyFn" class="title_name">我的回复</span></li><li  v-link="{path: \'/tellMe\'}">'
+      +'<div class="mag_number1" style="display:none;" ></div><span class="circular"></span><span class="title_name">@我的</span></li><li class="active" v-link="{path: \'/relevant\'}">'
+      +'<div class="mag_number1" v-if="book > 0">{{book}}</div><span class="circular"></span><span @click="setbookFn" class="title_name">作品相关</span></li><li  v-link="{path: \'/system\'}">'
+      +'<div class="mag_number1" v-if="system > 0">{{system}}</div><span class="circular"></span><span @click="setsystemFn" class="title_name">系统消息</span></li><li  v-link="{path: \'/official\'}">'
+      +'<div class="mag_number1" v-if="official > 0">{{official}}</div><span class="circular"></span><span @click="setofficialFn" class="title_name">官方公共</span></li></ul>'
       +'<div class="content"><div class="bookBlockList" "><div class="bookBlockList_title"><span class="hr"></span><span class="title">作品相关</span><span class="titleTwo">work related</span></div>'
       +'<div style="min-height:500px">'
       +'<div v-for="obj in bookList.info" ><div class="img-attention-word"><span class="attention-word1">{{obj.messageTitle}}</span><span class="attention-word2">{{ szxj.getLocalTimeTwo(obj.messageCreateDate) }}</span></div><p>{{obj.message}}</p><hr style="border: 1px solid #E1E1E1; margin:0 0 0 5%;" width="90%" /></div>'
@@ -33,6 +33,10 @@ var relevant = Vue.extend({
             n: 10,
             pageCount: 1,
           },
+          book:'',
+          official:'',
+          reply:'',
+          system:'',
         };
       },
       route: {
@@ -41,6 +45,18 @@ var relevant = Vue.extend({
         } 
       },
       methods: {
+        setbookFn: function(){
+          this.$set('book', 0);
+        },
+        setofficialFn: function(){
+          this.$set('official', 0);
+        },
+        setreplyFn: function(){
+          this.$set('reply', 0);
+        },
+        setsystemFn: function(){
+          this.$set('system', 0);
+        },
         setPageCount: function(v) {
           var obj = this.obj;
           obj.pageCount =  v;
@@ -64,6 +80,14 @@ var relevant = Vue.extend({
             this.$set('bookList', response.data);
             this.setPageCount(response.data.totalPage);
             console.log(this.bookList);
+          });
+          SZXJ.http(this,'get', PathList.getStatus, {}, (response) => {
+            this.userName = response.data.status.userName;
+            this.book = response.data.book;
+            this.official = response.data.official;
+            this.reply = response.data.reply;
+            this.system = response.data.system;
+            this.number1 = response.data.system + response.data.reply + response.data.official + response.data.book;
           });
         },
       },
